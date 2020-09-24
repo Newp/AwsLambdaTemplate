9623cc831df6c267535039d8fc941d24fd3f04f1
Newp Lee <mynewp@gmail.com>
2020-09-24 21:40:54 +0900

    참조용 프로젝트 추가, working-directory 수정

--- AwsLambdaTemplate/Controllers/ValuesController.cs
+++ AwsLambdaTemplate/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ReferenceTest;
 
 namespace AwsLambdaTemplate.Controllers
 {
@@ -13,7 +14,7 @@ namespace AwsLambdaTemplate.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", Sample.Message };
         }
 
         // GET api/values/5
---
+++ ReferenceTest/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ReferenceTest
+{
+    public static class Sample
+    {
+        public static readonly string Message = "Message for reference test";
+    }
+}
