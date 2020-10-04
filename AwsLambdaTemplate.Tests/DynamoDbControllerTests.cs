ad270773503cc6c1385101a36fb8b9c1a6233c83
Newp Lee <mynewp@gmail.com>
2020-10-04 21:06:23 +0900

    dynamodb 기본테스트 추가

---
+++ AwsLambdaTemplate.Tests/DynamoDbControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AwsLambdaTemplate.Tests
+{
+    public class DynamoDbControllerTests : ServerFixture
+    {
+        HttpClient client;
+
+        public DynamoDbControllerTests()
+        {
+            this.client = base.CreateClient();
+        }
+
+        [Fact]
+        public async Task ConnectionTest()
+        {
+            var result = await client.GetAsync("/api/dynamodb");
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, result.StatusCode);
+
+            var responseBody = await result.Content.ReadAsStringAsync();
+            Assert.Equal("dynamodb ok", responseBody);
+        }
+    }
+}
---
+++ AwsLambdaTemplate.Tests/ServerFixture.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AwsLambdaTemplate.Tests
+{
+    public class ServerFixture
+    {
+        public TestServer testServer = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>()
+                .ConfigureTestServices(collection =>
+                {
+                    
+                }));
+
+        public HttpClient CreateClient() => this.testServer.CreateClient();
+    }
+}
